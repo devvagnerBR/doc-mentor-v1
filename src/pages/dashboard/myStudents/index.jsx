b0f0e8886d0d97ecb632fe7d34a3ac4ce489cb35
devvagner <wagnerluizsg@outlook.com.br>
2023-05-18 19:23:45 -0300

    fix 0.6

--- src/pages/dashboard/myStudents/index.jsx
+++ src/pages/dashboard/myStudents/index.jsx
@@ -27,7 +27,7 @@ const MyStudents = () => {
         await maskStudentAsInactive( student.id )
     }
 
-    
+
     const handleStudentDetails = ( student ) => {
         window.localStorage.setItem( 'student',JSON.stringify( student ) )
         setStudent( student )
@@ -47,73 +47,76 @@ const MyStudents = () => {
             </header>
 
             <main className='w-full h-full flex justify-start items-start gap-7 px-4'>
-                <div>
-                    <p className='text-neutral-300 font-light '>Num</p>
-                    {students && students.map( ( student,index ) => {
-                        return <p className={`${student.status === 'inativo' && 'text-neutral-400'}  text-sm`} key={student.id} >{index + 1}</p>
-                    } )}
-                </div>
-                <div className=''>
-                    <p className='text-neutral-300 font-light '>Nome</p>
-                    {students && students.map( ( student ) => {
-                        return <p
-                            key={student.id}
-                            className={`${student.status === 'inativo' && 'text-neutral-400'} underline cursor-pointer flex text-sm`}
-                            onClick={() => handleStudentDetails( student )}
-                        >
-                            {student.student_name}
-                        </p>
-                    } )}
-                </div>
+                {students.length === 0 ? 'nenhum aluno cadastrado' :
+                    <>
 
-                <div className=''>
-                    <p className='text-neutral-300 font-light '>Idade</p>
+                    <div>
+                        <p className='text-neutral-300 font-light '>Num</p>
+                        {students && students.map( ( student,index ) => {
+                            return <p className={`${student.status === 'inativo' && 'text-neutral-400'}  text-sm`} key={student.id} >{index + 1}</p>
+                        } )}
+                    </div>
+                    <div className=''>
+                        <p className='text-neutral-300 font-light '>Nome</p>
+                        {students && students.map( ( student ) => {
+                            return <p
+                                key={student.id}
+                                className={`${student.status === 'inativo' && 'text-neutral-400'} underline cursor-pointer flex text-sm`}
+                                onClick={() => handleStudentDetails( student )}
+                            >
+                                {student.student_name}
+                            </p>
+                        } )}
+                    </div>
 
-                    {students && students.map( ( student,index ) => {
-                        return <p className={`${student.status === 'inativo' && 'text-neutral-400'}  text-sm`} key={student.id} >{student.age} anos</p>
-                    } )}
-                </div>
+                    <div className=''>
+                        <p className='text-neutral-300 font-light '>Idade</p>
 
-
-
-                <div className=''>
-                    <p className='text-neutral-300 font-light '>Escola</p>
-                    {students && students.map( ( student,index ) => {
-                        return <p className={`${student.status === 'inativo' && 'text-neutral-400'}  text-sm`} key={student.id}>{student.school_name}</p>
-                    } )}
-                </div>
-
-                <div className=''>
-                    <p className='text-neutral-300 font-light '>Deficiência</p>
-                    {students.map( ( student,index ) => {
-                        return <p className={`${student.status === 'inativo' && 'text-neutral-400'}  text-sm`} key={student.id}>{student.deficit}</p>
-                    } )}
-                </div>
-                <div className=''>
-                    <p className='text-neutral-300 font-light '>Dias de atendimento</p>
-                    {students.map( ( student,index ) => {
-                        return <p className={`${student.status === 'inativo' && 'text-neutral-400'}  text-sm`} key={student.id}>{student.service_days}</p>
-                    } )}
-                </div>
-                <div className=''>
-                    <p className='text-neutral-300 font-light '>Último relatório</p>
-                    {students.map( ( student,index ) => {
-                        return <p className={`${student.status === 'inativo' && 'text-neutral-400'}  text-sm`} key={student.id}>{student.last_report ? moment( student?.last_report ).fromNow() : 'nenhum'}</p>
-                    } )}
-                </div>
-                <div className='flex flex-col items-start justify-center   '>
-                    <p className='text-neutral-300 font-light  flex '>status</p>
-                    {students.map( ( student,index ) => {
-                        return <div key={student.id} className='flex gap-2 items-center'>
-                            <p className={` text-sm ${student.status === 'ativo' ? 'text-green-600' : 'text-red-400'}`} >{student.status}</p>
-                            {student.status !== 'inativo' && <TrashSimple title='Desativar aluno' onClick={() => handleMarkStudentAsInactive( student )} className='cursor-pointer flex text-neutral-500 ' />}
-                        </div>
-                    } )}
-
-                </div>
+                        {students && students.map( ( student,index ) => {
+                            return <p className={`${student.status === 'inativo' && 'text-neutral-400'}  text-sm`} key={student.id} >{student.age} anos</p>
+                        } )}
+                    </div>
 
 
-            </main >
+
+                    <div className=''>
+                        <p className='text-neutral-300 font-light '>Escola</p>
+                        {students && students.map( ( student,index ) => {
+                            return <p className={`${student.status === 'inativo' && 'text-neutral-400'}  text-sm`} key={student.id}>{student.school_name}</p>
+                        } )}
+                    </div>
+
+                    <div className=''>
+                        <p className='text-neutral-300 font-light '>Deficiência</p>
+                        {students.map( ( student,index ) => {
+                            return <p className={`${student.status === 'inativo' && 'text-neutral-400'}  text-sm`} key={student.id}>{student.deficit}</p>
+                        } )}
+                    </div>
+                    <div className=''>
+                        <p className='text-neutral-300 font-light '>Dias de atendimento</p>
+                        {students.map( ( student,index ) => {
+                            return <p className={`${student.status === 'inativo' && 'text-neutral-400'}  text-sm`} key={student.id}>{student.service_days}</p>
+                        } )}
+                    </div>
+                    <div className=''>
+                        <p className='text-neutral-300 font-light '>Último relatório</p>
+                        {students.map( ( student,index ) => {
+                            return <p className={`${student.status === 'inativo' && 'text-neutral-400'}  text-sm`} key={student.id}>{student.last_report ? moment( student?.last_report ).fromNow() : 'nenhum'}</p>
+                        } )}
+                    </div>
+                    <div className='flex flex-col items-start justify-center   '>
+                        <p className='text-neutral-300 font-light  flex '>status</p>
+                        {students.map( ( student,index ) => {
+                            return <div key={student.id} className='flex gap-2 items-center'>
+                                <p className={` text-sm ${student.status === 'ativo' ? 'text-green-600' : 'text-red-400'}`} >{student.status}</p>
+                                {student.status !== 'inativo' && <TrashSimple title='Desativar aluno' onClick={() => handleMarkStudentAsInactive( student )} className='cursor-pointer flex text-neutral-500 ' />}
+                            </div>
+                        } )}
+
+                    </div>
+
+                </>}
+        </main>
 
         </div >
     )
