24c47b0db64d145d8f773abee1d197a10d6ec7ee
devvagner <wagnerluizsg@outlook.com.br>
2023-05-18 10:06:07 -0300

    fix 0.4

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -19,7 +19,7 @@ const Header = () => {
                     <h1 className='text-xs text-neutral-700 leading-[5px] font-Saira'>Bem vindo (a)</h1>
                     <h1 className='text-sm text-neutral-800 font-medium font-Saira'>{teacher?.name}</h1>
                 </div>
-                <div className='w-9 h-9 flex rounded-full overflow-hidden items-center justify-center shadow-sm cursor-pointer'>
+                <div className='w-9 h-9 flex rounded-full overflow-hidden items-center justify-center shadow-sm cursor-pointer mr-3'>
                     <img className='flex items-center justify-center rounded-full overflow-hidden' src={teacher?.avatar} alt="" />
                 </div>
             </div>
